8d52051d33623e8595d6640cb32920e34d93c8f3
Edson Junior <efojunior25@gmail.com>
2022-09-07 18:12:14 -0300

    atualizações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FullBannerComponent } from './shared/full-banner/full-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FullBannerComponent,
   ],
   imports: [
     BrowserModule,
